8a666ac1a8c8673bd783bb91ebef8caaa7008061
Andre Le <andrele2512@gmail.com>
2019-01-24 00:07:38 -0800

    added jump and slippery

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,8 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D playerRigidBody;
     public float speed;
-
+    public float jump;
+    bool canJump = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +20,50 @@ public class Player : MonoBehaviour
         playerRigidBody = GetComponent<Rigidbody2D>();
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        jump = 10F;
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            playerRigidBody.velocity = new Vector2(-speed, playerRigidBody.velocity.y);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
 
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jump);
+            canJump = false;
+        }
+
+
+    }
+
+    void Move()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        Vector2 currentVelocity = playerRigidBody.velocity;
+        playerRigidBody.velocity = new Vector2(movementX * speed, currentVelocity.y);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Vector2 feetVector = new Vector2(this.transform.position.x, 
+                this.transform.position.y - this.GetComponent<Collider2D>().bounds.extents.y);
+            
+            if (collision.GetContact(0).point.y < feetVector.y)
+            {
+                canJump = true;
+            }
+
+            
         }
     }
+
+
 }
+
+

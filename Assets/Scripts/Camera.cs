8f8fba3793f8c5a0b1799114216cb78a27455fd2
Unknown <andrele2512@gmail.com>
2019-01-24 18:56:41 -0800

    added gems

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject p1_stand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 player = p1_stand.transform.position;
+        transform.position = new Vector3(player.x, player.y, -10);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject RedGemPrefab;
+    public GameObject BlueGemPrefab;
     private Rigidbody2D playerRigidBody;
-    public float speed;
-    public float jump;
-    bool canJump = true;
+    private Collider2D playerCollider;
+    private float speed = 6f;
+    private float jump = 11f;
+    private bool canJump = false;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerRigidBody = GetComponent<Rigidbody2D>();
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        jump = 10F;
-
-
+        //playerRigidBody = GetComponent<Rigidbody2D>();
+        //playerCollider = GetComponent<Collider2D>();
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+        
         Move();
 
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jump);
-            canJump = false;
+            //playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jump);
+            //canJump = false;
+            Jump();
         }
 
+    }
 
+    void Jump()
+    {
+        canJump = false;
+        playerRigidBody.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
     }
 
     void Move()
@@ -44,17 +53,29 @@ public class Player : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        if (collider.CompareTag("BlueGem"))
+        {
+            Destroy(collider.gameObject);
+            Vector2 spot = new Vector2(-2, -1.5F);
+            Instantiate(RedGemPrefab, spot, Quaternion.identity);
+        }
+
+        if (collider.CompareTag("RedGem"))
+        {
+            Destroy(collider.gameObject);
+            Vector2 second_spot = new Vector2(-17.61F, -1.36F);
+            Instantiate(BlueGemPrefab, second_spot, Quaternion.identity);
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
-            Vector2 feetVector = new Vector2(this.transform.position.x, 
-                this.transform.position.y - this.GetComponent<Collider2D>().bounds.extents.y);
-            
-            if (collision.GetContact(0).point.y < feetVector.y)
+            Vector2 feetVector = new Vector2(this.transform.position.x, playerCollider.bounds.min.y);
+            RaycastHit2D hit = Physics2D.Raycast(feetVector, Vector2.down, 0.1f);
+       
+            if (hit && hit.collider.CompareTag("Ground"))
             {
                 canJump = true;
             }
@@ -63,6 +84,13 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            //canJump = false;
+        }
+    }
 
 }
 
